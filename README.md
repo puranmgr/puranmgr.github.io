cf9116329fe87f263440402e530531ba25861252
puranmgr <puranmgr@gmail.com>
2020-11-01 12:31:14 +0000

    hope this fixes it?

---
+++ README.md
@@ -0,0 +1,1 @@
+# puranpun.com
---
+++ js/scripts.js
@@ -0,0 +1,29 @@
+(function ($) {
+
+    // Remove no-js class
+    $('html').removeClass('no-js');
+
+    // Scroll to top
+    $('#to-top').click(function () {
+        $('html, body').animate({
+            scrollTop: 0
+        }, 500);
+    });
+
+    // Scroll to first element
+    $('#lead-down span').click(function () {
+        var scrollDistance = $('#lead').next().offset().top;
+        $('html, body').animate({
+            scrollTop: scrollDistance + 'px'
+        }, 500);
+    });
+
+    // Load additional projects
+    $('#view-more-projects').click(function (e) {
+        e.preventDefault();
+        $(this).fadeOut(300, function () {
+            $('#more-projects').fadeIn(300);
+        });
+    });
+
+})(jQuery);
